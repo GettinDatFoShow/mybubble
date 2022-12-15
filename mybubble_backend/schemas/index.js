c92d59f0537cab9bb51b240f42576cad50cebb8b
Robert Morris <robert.dale.morris@gmail.com>
2022-12-15 15:26:11 -0500

    installed tailwind

--- mybubble_backend/schemas/index.js
+++ mybubble_backend/schemas/index.js
@@ -1,5 +1,13 @@
+import comment from './comment';
+import pin from './pin';
+import postedBy from './postedBy';
+import save from './save';
 import user from './user.js';
 
 export const schemaTypes = [
+    comment,
+    pin,
+    postedBy,
+    save,
     user
 ]
